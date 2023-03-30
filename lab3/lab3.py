46b2cfc508ca5d3a356904d0d87b315259a14e4f
InTeRrUpTeR <shamanking1601@gmail.com>
2023-03-30 13:04:39 +0300

    то что есть

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,30 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import poisson
+from statsmodels.distributions import ECDF
 
 numbers = np.arange(0, 100)  # Массив от 0 до 99 включительно
-
-pmf = poisson.pmf(numbers, mu=1)
-pmf = np.round(pmf, 5)
-print(pmf)
-
-plt.plot(numbers, pmf)
-plt.title('Функция вероятности')
-plt.show()
-
-cdf = poisson.cdf(numbers, mu=1)
-cdf = np.round(cdf, 5)
-print(cdf)
-
-plt.plot(numbers, cdf)
-plt.title('Функция распределения')
-plt.show()
 
 
 def draw_plot(x, y):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.plot(x, y)
-    ax.set_xlim([-1, 10])
-    ax.set_ylim([-1, 2])
+    ax.set_xlim([0, 20])
+    ax.set_ylim([0, 0.4])
     plt.show()
+
+
+def draw_plt(x, y):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(x, y)
+    ax.set_xlim([0, 20])
+    ax.set_ylim([0, 1.2])
+    plt.show()
+
+
+pmf = poisson.pmf(numbers, mu=5)
+pmf = np.round(pmf, 5)
+print(pmf)
+draw_plot(numbers,pmf)
+
+# cdf = poisson.cdf(numbers, mu=1)
+# cdf = np.round(cdf, 5)
+# print(cdf)
+# draw_plt(numbers,cdf)
+
+s = poisson.rvs(mu=5, size=100)
+print(s)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+bins = list(np.arange(0, 20, 1))
+plt.hist(s, bins=bins, color='lightgreen', edgecolor='black', rwidth=0.9)
+plt.grid(axis='y', alpha=0.75)
+plt.show()
+
+
+# pmf = poisson.pmf(s, mu=1)
+# ecdf = ECDF(s)
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.plot(ecdf.x, ecdf.y, marker='.', linestyle='dotted')
+# ax.plot(bins, pmf, color='orange')
+# plt.tight_layout()
+# plt.show()
