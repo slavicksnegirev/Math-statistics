b74ddf6dd0922cad2242e6219092d8d442b0cf3d
InTeRrUpTeR <shamanking1601@gmail.com>
2023-04-25 23:22:24 +0300

    lab5

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -5,7 +5,6 @@ from statsmodels.distributions import ECDF
 
 numbers = np.arange(0, 100)  # Массив от 0 до 99 включительно
 
-
 def draw_plot(x, y):
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -16,13 +15,8 @@ def draw_plot(x, y):
 
 pmf = poisson.pmf(numbers, mu=5)
 pmf = np.round(pmf, 5)
-print(pmf)
+# print(pmf)
 draw_plot(numbers,pmf)
-
-# cdf = poisson.cdf(numbers, mu=1)
-# cdf = np.round(cdf, 5)
-# print(cdf)
-# draw_plt(numbers,cdf)
 
 s = poisson.rvs(mu=5, size=100)
 print(s)
@@ -34,13 +28,3 @@ bins = list(np.arange(0, 20, 1))
 plt.hist(s, bins=bins,density=True, color='lightgreen', edgecolor='black', rwidth=1)
 plt.grid(axis='y', alpha=0.75)
 plt.show()
-
-
-# pmf = poisson.pmf(s, mu=1)
-# ecdf = ECDF(s)
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# ax.plot(ecdf.x, ecdf.y, marker='.', linestyle='dotted')
-# ax.plot(bins, pmf, color='orange')
-# plt.tight_layout()
-# plt.show()
