fdce60c42185fb4d456c733159e40837cec69d4c
InTeRrUpTeR <shamanking1601@gmail.com>
2023-05-03 12:44:43 +0300

    lab7,8,9

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,15 +1,19 @@
+import math
+
 import numpy as np
 from scipy.stats import poisson
 from scipy import stats
+import pandas as pd
 
-numbers = np.arange(0, 100)  # Массив от 0 до 99 включительно
-selection = poisson.rvs(mu=5, size=100)
+size = 1000
+# numbers = np.arange(0, 1000)  # Массив от 0 до 99 включительно
+selection = poisson.rvs(mu=5, size=size)
 print(selection)
 # точечныые оценки
 mean = np.mean(selection)
 var = np.var(selection)
 print("Мат. ожидание : " ,mean)
-print("Дисперсия : " ,np.round(var,5))
+print("Дисперсия : " ,var)
 
 # Стандартное отклонение выборочной средней
 sigma_of_mean = stats.sem(selection)
@@ -17,34 +21,28 @@ print("Стандартное отклонение : " , sigma_of_mean)
 gamma = 0.95
 
 t1 = poisson.ppf((1+gamma)/2,mean,sigma_of_mean)
-e = np.sqrt(var/100) * t1
-e1 = np.sqrt(2/99) * var * t1
+e = np.sqrt(var/size) * t1
+e1 = np.sqrt(2/(size-1)) * var * t1
 print("Приближенный интервал для мат. ожидания: [", mean-e, ";", mean+e, "]")
 print("Приближенный интервал для дисперсии: [", var - e1, ";", var + e1, "]")
 
 interval = stats.norm.interval(gamma, loc=np.mean(selection), scale=stats.sem(selection))
 print("Точный интервал для мат. ожидания: [", interval)
 
-t3 = stats.chi2.ppf((1-gamma)/2, 99)
-t4 = stats.chi2.ppf((1+gamma)/2, 99)
-dxl1 = var * 99 / t4
-dxr1 = var * 99 / t3
+t3 = stats.chi2.ppf((1-gamma)/2, (size-1))
+t4 = stats.chi2.ppf((1+gamma)/2, (size-1))
+dxl1 = var * (size-1) / t4
+dxr1 = var * (size-1) / t3
 print("Точный интервал для дисперсии: [", dxl1, ";", dxr1, "]")
 
 
 
 
 mMoment = stats.moment(selection, 2)
-print("Метод моментов :", np.round(mMoment,5))
+print("Метод моментов :", mMoment)
 
 max_mean , max_std = stats.norm.fit(selection)
 print("Метод максимального правдоподобия :\n" 
       "\tМат. ожидание: ", max_mean ,
-      "\n\tДисперсия: ", np.round(pow(max_std,2),5))# метод максимального правдоподобия
+      "\n\tДисперсия: ", max_std**2)# метод максимального правдоподобия
 
-
-
-print('Доверительный интервал для математического ожидания: ', end='')
-print(*stats.t.interval(0.95, len(selection), np.mean(selection), stats.sem(selection)), sep='-')
-print('Доверительный интервал для дисперсии: ', end='')
-print(*stats.t.interval(0.95, len(selection), np.var(selection), stats.sem(selection)), sep='-')
